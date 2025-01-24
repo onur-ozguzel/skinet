6d27b6754faa4c4af9b23993a166adf89cf7fe44
Onur Ozguzel <ozguzel.onur@gmail.com>
2025-01-24 18:18:16 +0100

    End of section 5

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -16,6 +16,22 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
 
     public bool IsDistinct { get; private set; }
 
+    public int Take { get; private set; }
+
+    public int Skip { get; private set; }
+
+    public bool IsPagingEnabled { get; private set; }
+
+    public IQueryable<T> AppyCriteria(IQueryable<T> query)
+    {
+        if (Criteria != null)
+        {
+            query = query.Where(Criteria);
+        }
+
+        return query;
+    }
+
     protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
     {
         OrderBy = orderByExpression;
@@ -30,13 +46,20 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
     {
         IsDistinct = true;
     }
+
+    protected void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+        IsPagingEnabled = true;
+    }
 }
 
 public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria)
     : BaseSpecification<T>(criteria), ISpecification<T, TResult>
 {
     protected BaseSpecification() : this(null) { }
-    
+
     public Expression<Func<T, TResult>>? Select { get; private set; }
 
     protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
