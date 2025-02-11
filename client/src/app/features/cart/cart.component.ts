cda3dd2d1d51f12192e0f79223234f7d53745c51
Onur Ozguzel <ozguzel.onur@gmail.com>
2025-02-11 14:43:21 +0100

    End of section 19

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,23 +1,29 @@
-import { AfterViewChecked, Component, DoCheck, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
 import { EmptyStateComponent } from '../../shared/components/empty-state/empty-state.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   imports: [
     CartItemComponent,
     OrderSummaryComponent,
-    EmptyStateComponent
+    EmptyStateComponent,
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
+  private router = inject(Router);
   get itemCount(): number {
     return this.cartService.itemCount() ?? 0;
   }
 
   cartService = inject(CartService);
+
+  onAction() {
+    this.router.navigateByUrl('/shop');
+  }
 }
